d79a800ed4b61f317d713c0dec106e10110dfd93
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-02-14 18:47:29 -0500

    Add @phosphor-icons/react package and update Button component
    The @phosphor-icons/react package has been added to the dependencies in package.json and package-lock.json files. This new dependency is used in the Button component, where a PlusCircle icon from this package is utilized. The CSS for the Button component has also been adjusted to accommodate this new icon.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,11 @@
 import styles from "./Button.module.css";
+import { PlusCircle } from "@phosphor-icons/react";
 
 export const Button = () => {
-  return <button className={styles.button}>Criar</button>;
+  return (
+    <button className={styles.button}>
+      Criar
+      {<PlusCircle size={18} color="#f2f2f2" weight="bold" />}
+    </button>
+  );
 };
