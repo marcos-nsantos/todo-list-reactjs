6c5d855e2c15fc3682edef6dfb3bbf15f7f8bbec
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-02-17 18:37:04 -0500

    Implement task status toggle functionality
    Added the ability to alter the status of a task from unfinished to finished and vice versa in Item.tsx component. Incorporated useState to manage task status state, functions to render different styles based on whether a task is done or not, and an onClick event to change event status. Modified corresponding styles in Item.module.css to support this functionality.

--- src/components/task-list/Item.tsx
+++ src/components/task-list/Item.tsx
@@ -1,18 +1,39 @@
 import styles from "./Item.module.css";
-import { Circle, Trash } from "@phosphor-icons/react";
+import { CheckCircle, Circle, Trash } from "@phosphor-icons/react";
+import { useState } from "react";
 
 export function Item() {
+  const [isDone, setIsDone] = useState(false);
+
+  function handleChangeTaskStatus() {
+    setIsDone(!isDone);
+  }
+
+  function renderIcon() {
+    if (!isDone) {
+      return <Circle size={18} weight="bold" color="#4ea8d3" />;
+    }
+    return <CheckCircle size={18} weight="fill" color="#5e60ce" />;
+  }
+
+  function renderText(text: string) {
+    const textClassName = isDone
+      ? `${styles.paragraph} ${styles["text-line-through"]}`
+      : styles.paragraph;
+    return <p className={textClassName}>{text}</p>;
+  }
+
   return (
     <div className={styles.item}>
       <div>
         <label htmlFor="item">
           <input type="checkbox" />
-          <span className={styles.checkbox}>
-            <Circle size={18} weight="bold" color="#4ea8d3" />
+          <span className={styles.checkbox} onClick={handleChangeTaskStatus}>
+            {renderIcon()}
           </span>
         </label>
 
-        <p className={styles.paragraph}>Terminar esse projeto</p>
+        {renderText("Terminar esse projeto")}
       </div>
 
       <button>{<Trash size={16} color="#808080" />}</button>
