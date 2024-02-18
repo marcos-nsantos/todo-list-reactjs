e8cb38d97a3e1dab1f73a23177a197164bd6be9a
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-02-18 09:06:42 -0500

    Refactor form components into a single module
    Removed the individual Input and Button components and their styling. Instead, merged their functionality and styles into the Form component. This reduces component fragmentation and increases maintainability.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,19 @@
 import styles from "./Form.module.css";
-import { Input } from "./Input.tsx";
-import { Button } from "./Button.tsx";
+import { PlusCircle } from "@phosphor-icons/react";
 
 export const Form = () => {
   return (
-    <div className={styles.formGroup}>
-      <Input />
-      <Button />
-    </div>
+    <form className={styles.formGroup}>
+      <input
+        className={styles.input}
+        type="text"
+        placeholder="Adicione uma nova tarefa"
+      />
+
+      <button className={styles.button}>
+        Criar
+        {<PlusCircle size={18} color="#f2f2f2" weight="bold" />}
+      </button>
+    </form>
   );
 };
