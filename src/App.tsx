c495db374ab074487a101c8bdbece98363d252f4
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-02-18 09:43:47 -0500

    Refactor task list implementation and move to App component
    This commit moves the Tasks functionality straight into the App component, eliminating the need for a separate Tasks component and its associated styles. The styles and task handling logic have been transferred from Tasks to the App component's CSS module and TSX file respectively. This refactoring simplifies the component structure.

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,64 @@ import "./Global.css";
 import styles from "./App.module.css";
 import { Header } from "./components/Header.tsx";
 import { Form } from "./components/Form.tsx";
-import { Tasks } from "./components/task-list/Tasks.tsx";
+import { useState } from "react";
+import {
+  ITaskItem,
+  ITaskItemProp,
+  TaskItem,
+} from "./components/task-list/TaskItem.tsx";
+import { TaskListCounts } from "./components/task-list/TaskListCounts.tsx";
 
 function App() {
+  const [tasks, setTasks] = useState<ITaskItem[]>([]);
+
+  function handleAddNewTask(text: string) {
+    const newTask = {
+      id: new Date().getTime(),
+      text,
+      status: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
+  function handleDeleteTask(id: number) {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+  }
+
+  function handleToggleTaskStatus(id: number) {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, status: !task.status };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  const completedTasks = tasks.filter((task) => task.status);
+  const incompletedTasks = tasks.filter((task) => !task.status);
+  const allTasks = [...incompletedTasks, ...completedTasks];
+
+  function renderTasks() {
+    return allTasks.map((task) => {
+      const itemProps: ITaskItemProp = {
+        props: task,
+        onChangeStatus: handleToggleTaskStatus,
+        onDeleteTask: handleDeleteTask,
+      };
+      return <TaskItem key={task.id} {...itemProps} />;
+    });
+  }
+
   return (
     <>
       <Header />
+
       <main className={styles.content}>
         <Form />
-        <Tasks />
+        <TaskListCounts />
+        <div className={styles.listGroup}>{renderTasks()}</div>
       </main>
     </>
   );
